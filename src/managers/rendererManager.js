27d700d6429390df526042cc2cd3ec9660145675
Nikhil <nikhil@thesolarlabs.com>
2025-03-06 16:31:38 +0530

    renderer change

--- src/managers/rendererManager.js
+++ src/managers/rendererManager.js
@@ -4,8 +4,8 @@ export default class RendererManager {
     constructor() {
         this.renderer = new THREE.WebGLRenderer({
             antialias: true,
-            preserveDrawingBuffer: true,
-            logarithmicDepthBuffer: true,
+            // preserveDrawingBuffer: true,
+            // logarithmicDepthBuffer: true,
         });
         this.DPR = window.devicePixelRatio ? window.devicePixelRatio : 1;
         this.renderer.setPixelRatio(this.DPR);
